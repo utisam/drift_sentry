51de21fff01a4b62ca1389e2b6041cb9ef300b26
Masatoshi Tsushima <utisam@gmail.com>
2023-06-19 06:49:11 +0900

    Remove dependency for flutter (only dart)

--- test/sentry_executor_test.dart
+++ test/sentry_executor_test.dart
@@ -1,9 +1,9 @@
 import 'package:drift/native.dart';
 import 'package:drift_sentry/drift_sentry.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:sentry/sentry.dart';
 import 'package:sentry/src/sentry_tracer.dart';
+import 'package:test/test.dart';
 
 import '../example/database.dart';
 import 'mocks/mocks.mocks.dart';
